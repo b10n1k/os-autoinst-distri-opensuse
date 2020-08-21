dc8df16c0681e74bd78e91da5b03d5534b01ab02
ybonatakis <ybonatakis@suse.com>
2020-08-21 16:24:55 +0200

    test incomplete with with procude_on_failure

--- tests/console/zypper_lifecycle.pm
+++ tests/console/zypper_lifecycle.pm
@@ -44,7 +44,7 @@ sub run {
     select_console 'root-console';
     script_run('zypper ref');
     select_console 'user-console';
-    my $overview = script_output('zypper lifecycle', 600);
+    my $overview = script_output('zypper lifecycle', 600, proceed_on_failure => 1);
     die "Missing header line:\nOutput: '$overview'" unless $overview =~ /Product end of support/;
     die "Missing link to lifecycle page:\nOutput: '$overview'"
       if $overview =~ /n\/a/ && $overview !~ qr{\*\) See https://www.suse.com/lifecycle for latest information};
