6a1cf8115e10af661dc7da1e197aea1797e93c91
Ioannis Bonatakis <ybonatakis@suse.com>
2022-01-28 09:59:16 +0100

    Separate installation from actual test on ibtest
    The installation eats time and it can done in individual job. Trigger
    _ibtest-master_ and _ibtest-slave_ after installation is done.
    
    Signed-off-by: Ioannis Bonatakis <ybonatakis@suse.com>

--- tests/kernel/ibtests_barriers.pm
+++ tests/kernel/ibtests_barriers.pm
@@ -15,7 +15,7 @@ use lockapi;
 
 
 sub run {
-    barrier_create('IBTEST_SETUP', 2);
+    # barrier_create('IBTEST_SETUP', 2);
     barrier_create('IBTEST_BEGIN', 2);
     barrier_create('IBTEST_DONE', 2);
 }
